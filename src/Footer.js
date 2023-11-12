81dfdead24c371edb80a9dc4359fab09c8dbd344
mejkyss <124203408+mejkyss@users.noreply.github.com>
2023-11-12 12:44:47 +0100

    Add Footer and Header

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <p>
+      <a href="https://github.com/mejkyss/weather-app-react" target="_blank">
+        Open-source code
+      </a>{" "}
+      by Markéta Dokoupilová
+    </p>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import WeatherApp from "./WeatherApp";
+import Header from "./Header";
+import Footer from "./Footer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Header />
     <WeatherApp />
+    <Footer />
   </React.StrictMode>
 );
 
