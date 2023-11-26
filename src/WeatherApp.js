0bce8f56e122f022924199fc19185246c60f3e88
mejkyss <124203408+mejkyss@users.noreply.github.com>
2023-11-26 15:26:16 +0100

    Add basic CSS

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,59 +1,56 @@
-import React, { useState } from "react";
-import axios from "axios";
+// import React, { useState } from "react";
+// import axios from "axios";
+import "./WeatherApp.css";
+import React from "react";
 
 export default function WeatherApp() {
-  let [temperature, setTemperature] = useState("");
-  let [description, setDescription] = useState("");
-  let [humidity, setHumidity] = useState("");
-  let [wind, setWind] = useState("");
-  let [icon, setIcon] = useState("");
-
-  function updateInfo(response) {
-    setTemperature(Math.round(response.data.main.temp));
-    setDescription(response.data.weather[0].description);
-    setHumidity(response.data.main.humidity);
-    setWind(response.data.wind.speed);
-    setIcon(response.data.weather[0].icon);
-  }
-
-  let [city, setCity] = useState("oslo");
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3a94f3778290bfeee61278505dbbe51d&units=metric`;
-    axios.get(url).then(updateInfo);
-  }
-
-  function updateCity(event) {
-    setCity(event.target.value);
-  }
-
-  function displayInfo() {
-    if (temperature) {
-      return (
-        <ul>
-          <li>Temperature: {temperature}°C</li>
-          <li>Description: {description}</li>
-          <li>Humidity: {humidity}%</li>
-          <li>Wind: {wind} km/h</li>
-          <li>
-            <img
-              src={`https://openweathermap.org/img/wn/${icon}@2x.png`}
-              alt="Weather icon"
-            />
-          </li>
-        </ul>
-      );
-    } else {
-      return null;
-    }
-  }
-
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" placeholder="Enter a city..." onChange={updateCity} />
-      <input type="submit" value="Search" />
-      {displayInfo()}
-    </form>
+    <div className="WeatherApp">
+      <h1>Weather in Lisbon</h1>
+
+      {/* Beginning of search field */}
+      <form>
+        <div className="row search-field">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Search for a city"
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="button btn btn-primary"
+            />
+          </div>
+        </div>
+      </form>
+      {/* End of search field */}
+
+      <div class="container">
+        <div className="row">
+          <div className="col-6">
+            <ul>
+              <li>Wednesday 7:00</li>
+              <li>Mostly Cloudy</li>
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                alt="Mostly cloudy"
+              />
+              6°C
+            </ul>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Precipitation: 15%</li>
+              <li>Humidity: 72%</li>
+              <li>Wind: 13 km/h</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,14 +3,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import WeatherApp from "./WeatherApp";
-import Header from "./Header";
 import Footer from "./Footer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Header />
     <WeatherApp />
+    {/* Circles */}
     <Footer />
   </React.StrictMode>
 );
