5ba038427e25848c9e43e6f37f0a4a00d4b3a4a8
mejkyss <124203408+mejkyss@users.noreply.github.com>
2024-01-11 22:28:33 +0100

    Add 1 day forecast

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -9,6 +9,7 @@ import WeatherForecast from "./WeatherForecast";
 export default function WeatherApp(props) {
   const [ready, setReady] = useState(false);
   const [weatherData, setWeatherData] = useState({});
+  const [forecastData, setForecastData] = useState();
 
   function handleResponse(response) {
     setWeatherData({
@@ -22,14 +23,22 @@ export default function WeatherApp(props) {
       date: new Date(response.data.time * 1000),
       cityName: response.data.city,
     });
+
     setReady(true);
+  }
+
+  function handleForecastResponse(response) {
+    setForecastData(response.data.daily);
+    console.log(response.data);
   }
 
   function onSearch(city) {
     const apiKey = "f3b08f2120e855cotb88e29725334a5b";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-    axios.get(apiUrl).then(handleResponse);
-    return "Loading...";
+
+    const weatherDataUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(weatherDataUrl).then(handleResponse);
+    const forecastDataUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
+    axios.get(forecastDataUrl).then(handleForecastResponse);
   }
 
   if (ready) {
@@ -39,7 +48,8 @@ export default function WeatherApp(props) {
 
         <SearchField onSearch={onSearch} />
         <WeatherInfo weatherData={weatherData} />
-        <WeatherForecast />
+
+        <WeatherForecast forecastData={forecastData} ready={ready} />
       </div>
     );
   } else {
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,5 +1,47 @@
 import React from "react";
+import "./WeatherForecast.css";
 
-export default function WeatherForecast() {
-  return "Hello";
+export default function WeatherForecast(props) {
+  function getDay(timestamp) {
+    let index = new Date(timestamp).getDay();
+    let daysOfWeek = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    return daysOfWeek[index];
+  }
+  if (props.forecastData) {
+    return (
+      <div className="weather-forecast">
+        <div className="row">
+          <div className="col">
+            <div className="forecast-day">
+              {getDay(props.forecastData[0].time)}
+            </div>
+            <img
+              className="forecast-icon"
+              src={props.forecastData[0].condition.icon_url}
+              alt="Current weather icon"
+            />{" "}
+            <div className="forecast-temperature">
+              <span className="forecast-temperature-max">
+                {Math.round(props.forecastData[0].temperature.maximum)}°C
+              </span>
+              <span className="forecast-temperature-min">
+                {" "}
+                {Math.round(props.forecastData[0].temperature.minimum)}°C
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return "Hello";
+  }
 }
